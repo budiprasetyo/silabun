ec530fd021acb63c13331967e588e1ab15af97ee
metamorph <bprast1@yahoo.co.id>
2014-04-11 14:33:00 +0700

    adding admin rules and -get new method (untested source)

--- application/models/m_user.php
+++ application/models/m_user.php
@@ -33,13 +33,115 @@ class M_user extends MY_Model
 	public $rules 				= array(
 						'username' => array( 
 							'field' => 'username', 
-							'label' => 'username/email', 
-							'rules' => 'trim|required|xss_clean'
+							'label' => 'username', 
+							'rules' => 'trim|required|max_length[30]|xss_clean'
 						),
 						'password' => array( 
 							'field' => 'password', 
 							'label' => 'password', 
-							'rules' => 'trim|required'
+							'rules' => 'trim|required|max_length[128]'
+						)
+	);
+	public $rules_admin			= array(
+						'role_id' => array( 
+							'field' => 'role_id', 
+							'label' => 'role', 
+							'rules' => 'trim|required|is_natural|max_length[5]|xss_clean'
+						),
+						'email' => array( 
+							'field' => 'email', 
+							'label' => 'email', 
+							'rules' => 'trim|required|valid_email|callback__unique_email|max_length[120]|xss_clean'
+						),
+						'username' => array( 
+							'field' => 'username', 
+							'label' => 'username', 
+							'rules' => 'trim|required|max_length[30]|xss_clean'
+						),
+						'password_hash' => array( 
+							'field' => 'password_hash', 
+							'label' => 'password', 
+							'rules' => 'trim|matches[password_conf]|max_length[128]'
+						),
+						'password_conf' => array( 
+							'field' => 'password_conf', 
+							'label' => 'password confirmation', 
+							'rules' => 'trim|matches[password_hash]|max_length[128]'
+						),
+						'last_login' => array( 
+							'field' => 'last_login', 
+							'label' => 'last login', 
+							'rules' => 'trim|xss_clean'
+						),
+						'last_ip' => array( 
+							'field' => 'last_ip', 
+							'label' => 'last IP', 
+							'rules' => 'trim|max_length[40]|xss_clean'
+						),
+						'created_on' => array( 
+							'field' => 'created_on', 
+							'label' => 'created on', 
+							'rules' => 'trim|xss_clean'
+						),
+						'deleted' => array( 
+							'field' => 'deleted', 
+							'label' => 'deleted', 
+							'rules' => 'trim|max_length[1]|is_natural'
+						),
+						'reset_by' => array( 
+							'field' => 'reset_by', 
+							'label' => 'reset by', 
+							'rules' => 'trim|max_length[10]|xss_clean'
+						),
+						'banned' => array( 
+							'field' => 'banned', 
+							'label' => 'banned', 
+							'rules' => 'trim|max_length[1]|xss_clean'
+						),
+						'ban_message' => array( 
+							'field' => 'ban_message', 
+							'label' => 'ban message', 
+							'rules' => 'trim|max_length[255]|xss_clean'
+						),
+						'display_name' => array( 
+							'field' => 'display_name', 
+							'label' => 'display name', 
+							'rules' => 'trim|max_length[255]|xss_clean'
+						),
+						'display_name_changed' => array( 
+							'field' => 'display_name_changed', 
+							'label' => 'display name changed', 
+							'rules' => 'trim|max_length[10]|xss_clean'
+						),
+						'timezone' => array( 
+							'field' => 'timezone', 
+							'label' => 'timezone', 
+							'rules' => 'trim|max_length[4]|xss_clean'
+						),
+						'language' => array( 
+							'field' => 'language', 
+							'label' => 'language', 
+							'rules' => 'trim|max_length[20]|xss_clean'
+						),
+						'active' => array( 
+							'field' => 'active', 
+							'label' => 'active', 
+							'rules' => 'trim|is_natural|max_length[1]|xss_clean'
+						),
+						'activate_hash' => array( 
+							'field' => 'activate_hash', 
+							'label' => 'activate hash', 
+							'rules' => 'trim|max_length[40]|xss_clean'
+						),
+						'password_iterations' => array( 
+							'field' => 'password_iterations', 
+							'label' => 'password iterations', 
+							'rules' => 'trim|integer|max_length[4]|xss_clean'
+						),
+						'force_password_reset' => array( 
+							'field' => 'force_password_reset', 
+							'label' => 'force password reset', 
+							'rules' => 'trim|is_natural|max_length[1]|xss_clean'
 						)
 	);
 	
@@ -85,6 +187,21 @@ class M_user extends MY_Model
 	public function loggedin()
 	{
 		return (bool) $this->session->userdata('loggedin');
+	}
+	
+	public function get_new()
+	{
+		$user = new stdClass();
+		
+		$user->role_id	= '';
+		$user->role_id	= '';
+		$user->role_id	= '';
+		$user->role_id	= '';
+		$user->role_id	= '';
+		$user->role_id	= '';
+		$user->role_id	= '';
+		$user->role_id	= '';
+		
 	}
 	
 	public function hash($string)
