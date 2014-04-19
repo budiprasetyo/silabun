93a554ef03d44826b5a356e3d18e1601a39adbcb
metamorph <bprast1@yahoo.co.id>
2014-04-19 16:57:50 +0700

    adding session username logged in, and adding CKeditor method

--- application/libraries/Admin_Controller.php
+++ application/libraries/Admin_Controller.php
@@ -46,6 +46,9 @@ class Admin_Controller extends MY_Controller
 				'admin/user/logout'
 		);
 		
+		// user logged in
+		$this->data['username'] = $this->session->userdata('username');
+		
 		if(in_array(uri_string(), $exception_uris) == FALSE)
 		{
 			if ($this->m_user->loggedin() == FALSE) 
@@ -53,6 +56,54 @@ class Admin_Controller extends MY_Controller
 				redirect('admin/user/login');
 			}
 		}
+		
+		//CKEditor section
+		// load ckeditor helper
+		$this->load->helper('ckeditor');
+ 
+ 
+		//Ckeditor's configuration
+		$this->data['ckeditor'] = array(
+ 
+			//ID of the textarea that will be replaced
+			'id' 	=> 	'content',
+			'path'	=>	'assets/js/ckeditor',
+ 
+			//Optionnal values
+			'config' => array(
+				'toolbar' 	=> 	"Full", 	//Using the Full toolbar
+				'width' 	=> 	"650px",	//Setting a custom width
+				'height' 	=> 	'100px',	//Setting a custom height
+ 
+			),
+ 
+			//Replacing styles from the "Styles tool"
+			'styles' => array(
+ 
+				//Creating a new style named "style 1"
+				'style 1' => array (
+					'name' 		=> 	'Blue Title',
+					'element' 	=> 	'h2',
+					'styles' => array(
+						'color' 	=> 	'Blue',
+						'font-weight' 	=> 	'bold'
+					)
+				),
+ 
+				//Creating a new style named "style 2"
+				'style 2' => array (
+					'name' 	=> 	'Red Title',
+					'element' 	=> 	'h2',
+					'styles' => array(
+						'color' 		=> 	'Red',
+						'font-weight' 		=> 	'bold',
+						'text-decoration'	=> 	'underline'
+					)
+				)				
+			)
+		);
+ 
+		
 	}
 
 }
