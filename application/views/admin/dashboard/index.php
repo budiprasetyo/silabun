5a841b59e2892174703f2f64d8fd99b29f5f974a
metamorph <bprast1@yahoo.co.id>
2014-10-17 11:56:45 +0700

    adding label for LPJ Pengeluaran, Penerimaan and adding sparkline chart in LPJ Pengeluaran per item

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -1,5 +1,590 @@
  <!--Begin Datatables-->
 <div class="row">
+
+  <div class="col-lg-12">
+  
+	<div class="box">
+	  <header>
+		  <div class="form-group">
+				<label for="text3" class="control-label col-lg-12 bg-red"><h4>Dashboard LPJ Pengeluaran</h4></label>
+		  </div>
+	  </header>
+	</div><!--/box -->
+	
+	<div class="box">
+	  <header>
+		<h5>Pengiriman LPJ Pengeluaran</h5>
+	  </header>
+	  <div class="body" id="trigo" style="height: 250px;"></div>
+	</div><!--/box -->
+	
+	<div class="text-center">
+	  <ul class="stats_box">
+		<!-- uang persediaan -->
+		<li>
+			<div class="inlinesparkline sparkline bar_week">
+			<?php 
+			if (count($grouped)) 
+			{
+				foreach ( $grouped as $tahun => $groups ) 
+				{
+					if($tahun !== '')
+					{
+						foreach ($groups as $bulan => $results) 
+						{
+							
+							$jml_uang_persediaan_chart_perbulan = 0;
+							foreach ($results as $chart) 
+							{
+								$jml_uang_persediaan_chart_perbulan += $chart['uang_persediaan'];
+							}
+							// data up every month
+							echo $jml_uang_persediaan_chart_perbulan . ', ';
+						}
+					}
+				}
+			}
+			?>
+			</div>
+		  <div class="stat_text">
+			<strong>Rp <?php echo amount_format($jml_uang_persediaan); ?></strong> BP <br />Uang Persediaan
+				<?php 
+					if (count($grouped)) 
+					{
+						foreach ( $grouped as $tahun => $groups ) 
+						{
+							if($tahun !== '')
+							{
+								
+								$i = 0;
+								$len = count($groups);
+								foreach ($groups as $bulan => $results) 
+								{
+									$jml_uang_persediaan_chart_perbulan = 0;
+									foreach ($results as $chart) 
+									{
+										$jml_uang_persediaan_chart_perbulan += $chart['uang_persediaan'];
+									}
+									
+									// get last data in array and last data -1
+									if ($i === $len - 2) 
+									{
+										$before_last_up = $jml_uang_persediaan_chart_perbulan;
+									}
+									else if ($i === $len - 1) 
+									{
+										$last_up = $jml_uang_persediaan_chart_perbulan;
+										// calculation for getting percentation
+										$percent_up = (($last_up - $before_last_up) / $before_last_up) * 100;
+										if ($percent_up > 0) {
+				?>
+											<span class="percent up"> 
+											<br />
+											<i class="fa fa-caret-up"></i> 
+				<?php
+											echo round($percent_up, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										else {
+				?>
+											<span class="percent down"> 
+											<br />
+											<i class="fa fa-caret-down"></i> 
+				<?php
+												echo round($percent_up, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										
+										
+									}
+										
+									$i++;
+								}
+							}
+						}
+					}
+				?>
+		  </div>
+		</li>
+		
+		<!-- ls bendahara -->
+		<li>
+			<div class="inlinesparkline sparkline bar_week">
+			<?php 
+			if (count($grouped)) 
+			{
+				foreach ( $grouped as $tahun => $groups ) 
+				{
+					if($tahun !== '')
+					{
+						foreach ($groups as $bulan => $results) 
+						{
+							
+							$jml_ls_bendahara_chart_perbulan = 0;
+							foreach ($results as $chart) 
+							{
+								$jml_ls_bendahara_chart_perbulan += $chart['ls_bendahara'];
+							}
+							// data up every month
+							echo $jml_ls_bendahara_chart_perbulan . ', ';
+						}
+					}
+				}
+			}
+			?>
+			</div>
+		  <div class="stat_text">
+			<strong>Rp <?php echo amount_format($jml_ls_bendahara); ?></strong> BP <br />LS Bendahara
+				<?php 
+					if (count($grouped)) 
+					{
+						foreach ( $grouped as $tahun => $groups ) 
+						{
+							if($tahun !== '')
+							{
+								
+								$i = 0;
+								$len = count($groups);
+								foreach ($groups as $bulan => $results) 
+								{
+									$jml_ls_bendahara_chart_perbulan = 0;
+									foreach ($results as $chart) 
+									{
+										$jml_ls_bendahara_chart_perbulan += $chart['ls_bendahara'];
+									}
+									
+									// get last data in array and last data -1
+									if ($i === $len - 2) 
+									{
+										$before_last_ls = $jml_ls_bendahara_chart_perbulan;
+									}
+									else if ($i === $len - 1) 
+									{
+										$last_ls = $jml_ls_bendahara_chart_perbulan;
+										// calculation for getting percentation
+										$percent_ls = (($last_ls - $before_last_ls) / $before_last_ls) * 100;
+										if ($percent_ls > 0) {
+				?>
+											<span class="percent up"> 
+											<br />
+											<i class="fa fa-caret-up"></i> 
+				<?php
+											echo round($percent_ls, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										else {
+				?>
+											<span class="percent down"> 
+											<br />
+											<i class="fa fa-caret-down"></i> 
+				<?php
+												echo round($percent_ls, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										
+										
+									}
+										
+									$i++;
+								}
+							}
+						}
+					}
+				?>
+		  </div>
+		</li>
+		
+		<!-- pajak -->
+		<li>
+			<div class="inlinesparkline sparkline bar_week">
+			<?php 
+			if (count($grouped)) 
+			{
+				foreach ( $grouped as $tahun => $groups ) 
+				{
+					if($tahun !== '')
+					{
+						foreach ($groups as $bulan => $results) 
+						{
+							
+							$jml_pajak_chart_perbulan = 0;
+							foreach ($results as $chart) 
+							{
+								$jml_pajak_chart_perbulan += $chart['pajak'];
+							}
+							// data up every month
+							echo $jml_pajak_chart_perbulan . ', ';
+						}
+					}
+				}
+			}
+			?>
+			</div>
+		  <div class="stat_text">
+			<strong>Rp <?php echo amount_format($jml_pajak); ?></strong> BP <br />Pajak
+				<?php 
+					if (count($grouped)) 
+					{
+						foreach ( $grouped as $tahun => $groups ) 
+						{
+							if($tahun !== '')
+							{
+								
+								$i = 0;
+								$len = count($groups);
+								foreach ($groups as $bulan => $results) 
+								{
+									$jml_pajak_chart_perbulan = 0;
+									foreach ($results as $chart) 
+									{
+										$jml_pajak_chart_perbulan += $chart['pajak'];
+									}
+									
+									// get last data in array and last data -1
+									if ($i === $len - 2) 
+									{
+										$before_last_pajak = $jml_pajak_chart_perbulan;
+									}
+									else if ($i === $len - 1) 
+									{
+										$last_pajak = $jml_pajak_chart_perbulan;
+										// calculation for getting percentation
+										$percent_pajak = (($last_pajak - $before_last_pajak) / $before_last_pajak) * 100;
+										if ($percent_pajak > 0) {
+				?>
+											<span class="percent up"> 
+											<br />
+											<i class="fa fa-caret-up"></i> 
+				<?php
+											echo round($percent_pajak, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										else {
+				?>
+											<span class="percent down"> 
+											<br />
+											<i class="fa fa-caret-down"></i> 
+				<?php
+												echo round($percent_pajak, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										
+										
+									}
+										
+									$i++;
+								}
+							}
+						}
+					}
+				?>
+		  </div>
+		</li>
+		
+		<!-- pengeluaran lain -->
+		<li>
+			<div class="inlinesparkline sparkline bar_week">
+			<?php 
+			if (count($grouped)) 
+			{
+				foreach ( $grouped as $tahun => $groups ) 
+				{
+					if($tahun !== '')
+					{
+						foreach ($groups as $bulan => $results) 
+						{
+							
+							$jml_pengeluaran_lain_chart_perbulan = 0;
+							foreach ($results as $chart) 
+							{
+								$jml_pengeluaran_lain_chart_perbulan += $chart['pengeluaran_lain'];
+							}
+							// data up every month
+							echo $jml_pengeluaran_lain_chart_perbulan . ', ';
+						}
+					}
+				}
+			}
+			?>
+			</div>
+		  <div class="stat_text">
+			<strong>Rp <?php echo amount_format($jml_pengeluaran_lain); ?></strong> BP <br />Lain-Lain
+				<?php 
+					if (count($grouped)) 
+					{
+						foreach ( $grouped as $tahun => $groups ) 
+						{
+							if($tahun !== '')
+							{
+								
+								$i = 0;
+								$len = count($groups);
+								foreach ($groups as $bulan => $results) 
+								{
+									$jml_pengeluaran_lain_chart_perbulan = 0;
+									foreach ($results as $chart) 
+									{
+										$jml_pengeluaran_lain_chart_perbulan += $chart['pengeluaran_lain'];
+									}
+									
+									// get last data in array and last data -1
+									if ($i === $len - 2) 
+									{
+										$before_last_pengeluaran_lain = $jml_pengeluaran_lain_chart_perbulan;
+									}
+									else if ($i === $len - 1) 
+									{
+										$last_pengeluaran_lain = $jml_pengeluaran_lain_chart_perbulan;
+										// calculation for getting percentation
+										$percent_pengeluaran_lain = (($last_pengeluaran_lain - $before_last_pengeluaran_lain) / $before_last_pengeluaran_lain) * 100;
+										if ($percent_pengeluaran_lain > 0) {
+				?>
+											<span class="percent up"> 
+											<br />
+											<i class="fa fa-caret-up"></i> 
+				<?php
+											echo round($percent_pengeluaran_lain, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										else {
+				?>
+											<span class="percent down"> 
+											<br />
+											<i class="fa fa-caret-down"></i> 
+				<?php
+												echo round($percent_pengeluaran_lain, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										
+										
+									}
+										
+									$i++;
+								}
+							}
+						}
+					}
+				?>
+		  </div>
+		</li>
+		
+		
+		<!-- saldo -->
+		<li>
+			<div class="inlinesparkline sparkline bar_week">
+			<?php 
+			if (count($grouped)) 
+			{
+				foreach ( $grouped as $tahun => $groups ) 
+				{
+					if($tahun !== '')
+					{
+						foreach ($groups as $bulan => $results) 
+						{
+							
+							$jml_saldo_chart_perbulan = 0;
+							foreach ($results as $chart) 
+							{
+								$jml_saldo_chart_perbulan += $chart['saldo'];
+							}
+							// data up every month
+							echo $jml_saldo_chart_perbulan . ', ';
+						}
+					}
+				}
+			}
+			?>
+			</div>
+		  <div class="stat_text">
+			<strong>Rp <?php echo amount_format($jml_saldo); ?></strong> Saldo
+				<?php 
+					if (count($grouped)) 
+					{
+						foreach ( $grouped as $tahun => $groups ) 
+						{
+							if($tahun !== '')
+							{
+								
+								$i = 0;
+								$len = count($groups);
+								foreach ($groups as $bulan => $results) 
+								{
+									$jml_saldo_chart_perbulan = 0;
+									foreach ($results as $chart) 
+									{
+										$jml_saldo_chart_perbulan += $chart['saldo'];
+									}
+									
+									// get last data in array and last data -1
+									if ($i === $len - 2) 
+									{
+										$before_last_saldo = $jml_saldo_chart_perbulan;
+									}
+									else if ($i === $len - 1) 
+									{
+										$last_saldo = $jml_saldo_chart_perbulan;
+										// calculation for getting percentation
+										$percent_saldo = (($last_saldo - $before_last_saldo) / $before_last_saldo) * 100;
+										if ($percent_saldo > 0) {
+				?>
+											<span class="percent up"> 
+											<br />
+											<i class="fa fa-caret-up"></i> 
+				<?php
+											echo round($percent_saldo, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										else {
+				?>
+											<span class="percent down"> 
+											<br />
+											<i class="fa fa-caret-down"></i> 
+				<?php
+												echo round($percent_saldo, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										
+										
+									}
+										
+									$i++;
+								}
+							}
+						}
+					}
+				?>
+		  </div>
+		</li>
+		
+		<!-- kuitansi -->
+		<li>
+			<div class="inlinesparkline sparkline bar_week">
+			<?php 
+			if (count($grouped)) 
+			{
+				foreach ( $grouped as $tahun => $groups ) 
+				{
+					if($tahun !== '')
+					{
+						foreach ($groups as $bulan => $results) 
+						{
+							
+							$jml_kuitansi_chart_perbulan = 0;
+							foreach ($results as $chart) 
+							{
+								$jml_kuitansi_chart_perbulan += $chart['kuitansi'];
+							}
+							// data up every month
+							echo $jml_kuitansi_chart_perbulan . ', ';
+						}
+					}
+				}
+			}
+			?>
+			</div>
+		  <div class="stat_text">
+			<strong>Rp <?php echo amount_format($jml_kuitansi); ?></strong> Kuitansi
+				<?php 
+					if (count($grouped)) 
+					{
+						foreach ( $grouped as $tahun => $groups ) 
+						{
+							if($tahun !== '')
+							{
+								
+								$i = 0;
+								$len = count($groups);
+								foreach ($groups as $bulan => $results) 
+								{
+									$jml_kuitansi_chart_perbulan = 0;
+									foreach ($results as $chart) 
+									{
+										$jml_kuitansi_chart_perbulan += $chart['kuitansi'];
+									}
+									
+									// get last data in array and last data -1
+									if ($i === $len - 2) 
+									{
+										$before_last_kuitansi = $jml_kuitansi_chart_perbulan;
+									}
+									else if ($i === $len - 1) 
+									{
+										$last_kuitansi = $jml_kuitansi_chart_perbulan;
+										// calculation for getting percentation
+										$percent_kuitansi = (($last_kuitansi - $before_last_kuitansi) / $before_last_kuitansi) * 100;
+										if ($percent_kuitansi > 0) {
+				?>
+											<span class="percent up"> 
+											<br />
+											<i class="fa fa-caret-up"></i> 
+				<?php
+											echo round($percent_kuitansi, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										else {
+				?>
+											<span class="percent down"> 
+											<br />
+											<i class="fa fa-caret-down"></i> 
+				<?php
+												echo round($percent_kuitansi, 2);
+				?>
+											%
+											</span> 
+				<?php
+										}
+										
+										
+									}
+										
+									$i++;
+								}
+							}
+						}
+					}
+				?>
+		  </div>
+		</li>
+		
+	  </ul>
+	</div><!--/text-center -->
+  </div><!--/col-lg-12 -->
+  
+  
+  
   <div class="col-lg-12">
   <!-- Pengeluaran -->
 	<div class="box">
@@ -145,7 +730,16 @@
 	</div><!--/box-->
 	<!--/Pengeluaran -->
   
+	
 	<!-- Penerimaan -->
+	<div class="box">
+	  <header>
+		  <div class="form-group">
+				<label for="text3" class="control-label col-lg-12 bg-green dker"><h4>Dashboard LPJ Penerimaan</h4></label>
+		  </div>
+	  </header>
+	</div><!--/box -->
+	
 	<div class="box">
 	  <header>
 		<div class="icons">
