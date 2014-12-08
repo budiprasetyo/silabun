77ef490c2bfe68fea9eebfb1679ff9d0699d5917
metamorph <bprast1@yahoo.co.id>
2014-12-08 16:49:46 +0700

    adding port array in database config

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,8 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '127.0.0.1';
+$db['default']['port']     = '3306';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'djpbn@))*2008';
 $db['default']['database'] = 'dsp_lpj';
