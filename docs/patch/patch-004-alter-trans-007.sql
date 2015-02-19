f952d2703ce8a1d75a201d689ec80acc43653c81
metamorph <bprast1@yahoo.co.id>
2015-02-20 00:09:23 +0700

    alter table trans for rekening bendahara

--- docs/patch/patch-004-alter-trans-007.sql
+++ docs/patch/patch-004-alter-trans-007.sql
@@ -7,3 +7,13 @@ ADD COLUMN `kd_kppn` VARCHAR(3) NULL AFTER `id_ref_satker`,
 ADD COLUMN `nm_kppn` VARCHAR(200) NULL AFTER `kd_kppn`,
 ADD COLUMN `kd_kementerian` VARCHAR(3) NULL AFTER `nm_kppn`,
 ADD COLUMN `nm_kementerian` VARCHAR(175) NULL AFTER `kd_kementerian`;
+
+ALTER TABLE `dsp_lpj`.`t_lpjprek` 
+ADD COLUMN `id_ref_kppn` SMALLINT(5) UNSIGNED NULL AFTER `id_lpjprek`,
+ADD COLUMN `id_ref_kementerian` SMALLINT(5) UNSIGNED NULL AFTER `id_ref_kppn`,
+ADD COLUMN `id_ref_satker` SMALLINT(5) UNSIGNED NULL AFTER `id_ref_kementerian`,
+ADD COLUMN `nm_satker` VARCHAR(200) NULL AFTER `kd_satker`,
+ADD COLUMN `kd_kppn` VARCHAR(3) NULL AFTER `id_ref_satker`,
+ADD COLUMN `nm_kppn` VARCHAR(200) NULL AFTER `kd_kppn`,
+ADD COLUMN `kd_kementerian` VARCHAR(3) NULL AFTER `nm_kppn`,
+ADD COLUMN `nm_kementerian` VARCHAR(175) NULL AFTER `kd_kementerian`;
