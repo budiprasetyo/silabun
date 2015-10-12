604bce481b2bd00e559dae7d1c4ed2b4485e2f52
metamorph <bprast1@yahoo.co.id>
2015-10-12 09:10:09 +0700

    adding ORDER BY id for distinguishing rekening number between the first upload and the last upload at rekening penerimaan

--- application/models/m_upload.php
+++ application/models/m_upload.php
@@ -253,7 +253,9 @@ class M_upload extends MY_Model
 				kd_kppn 	= {$kd_kppn} AND
 				kd_satker 	= {$kd_satker} AND
 				tahun		= {$year} AND
-				bulan		= {$month}");
+				bulan		= {$month}
+			ORDER BY 
+				id_lpjprek DESC LIMIT 1");
 		// DB Sekretarian Rekening Penerimaan
 		$validate_rekening_penerimaan_sekretariat = $rekening_db->query("SELECT a.type, a.izinnum, a.izindate, a.bankcab, a.reknum, a.reknama, b.idbank, b.nama
 			FROM
